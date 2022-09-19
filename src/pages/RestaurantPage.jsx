df122bb950e8af4d554e090565829dad96abdd4c
Cazpian Levén <cazpians@gmail.com>
2022-09-19 19:38:50 +0200

    Created RestaurantInfoCard

--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
+import RestaurantInfoCard from '../components/RestaurantInfoCard'
 
 const RestaurantPage = () => {
     return (
         <div className="container mx-auto flex justify-center">
-            <div className="border-2 border-blue-800 bg-blue-400 w-1/2 rounded-xl">
-                <div className="flex justify-between">
-                    <h1 className="text-3xl m-2">Restaurant name</h1>
-                    <div className="text-xl m-3">❌</div>
-                </div>
-                <div className="flex justify-around w-8/12">
-                    <div className="border-2 border-blue-800 rounded-md p-1">Facebook</div>
-                    <div className="border-2 border-blue-800 rounded-md p-1">Hemsida</div>
-                    <div className="border-2 border-blue-800 rounded-md p-1">Instagram</div>
-                    <div className="border-2 border-blue-800 rounded-md p-1">Email</div>
-                </div>
-                <div className="m-3">
-                    <p>Beskrivning</p>
-                </div>
-                <br />
-                <p className="m-3">Adress: </p>
-                <p className="m-3">Ort: </p>
-                <p className="m-3">Telefon: </p>
-                <p className="m-3">Vegetarianskt: </p>
-            </div>
+            <RestaurantInfoCard />
         </div>
     )
 }
