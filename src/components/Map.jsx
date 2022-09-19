3c68da5941c892aca83dba9c9ca46fa01d4bd01e
Maria Lundhammar <marialundhammar@gmail.com>
2022-09-19 13:18:20 +0200

    adding a map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,1 +1,47 @@
-//here will be map component :D 
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
+
+
+const Map = () => {
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyDjwKG-ZAmhe10gH1w3vUBkviGJk1CrFWw"
+    })
+  
+ 
+  
+  return isLoaded && (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={coords}
+        zoom={15}
+        >
+       
+        <Marker
+        icon={{
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 7,
+         
+        }}
+        position={coords}
+        label = "Maria"
+        />
+ 
+        <Marker
+        icon={{
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 7,
+        }}
+        position={coords2}
+        label = "Alexander"
+        />
+  
+      </GoogleMap>
+  )
+ 
+ }
+ 
+ export default Map
+   
---
+++ src/hooks/useMap.jsx
@@ -0,0 +1,29 @@
+
+
+const useMap = () => {
+    //Just trying with some coords 
+    const coords =  {
+    lat: 55.593242405381694,
+    lng:13.016352876632064,
+    }
+ 
+   
+    const coords2 =  {
+     lat:55.58477609505269,
+     lng:13.011208857604752,
+    }
+
+    //style of map container
+   const containerStyle = {
+    width: "100em",
+    height: '100em'
+    };  
+  
+  
+
+
+
+}
+
+
+export default useMap
