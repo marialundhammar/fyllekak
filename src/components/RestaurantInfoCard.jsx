b3c4b395aae4f661ef18e425aebc656a5fd09845
Cazpian Levén <cazpians@gmail.com>
2022-09-19 21:39:55 +0200

    Implemented useRestaurants hook successfully on RestaurantInfoCard

--- src/components/RestaurantInfoCard.jsx
+++ src/components/RestaurantInfoCard.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 
-const RestaurantInfoCard = () => {
+const RestaurantInfoCard = ({ restaurant }) => {
+    console.log('restaurant ==>', restaurant)
+
     return (
         <div className="border-2 border-blue-800 bg-blue-400 w-1/2 rounded-xl">
             <div className="flex justify-between">
-                <h1 className="text-3xl m-2">Restaurant name</h1>
+                <h1 className="text-3xl m-2">{restaurant.namn}</h1>
                 <div className="text-xl m-3">❌</div>
             </div>
             <div className="flex justify-around w-8/12">
-                <div className="border-2 border-blue-800 rounded-md p-1">Facebook</div>
-                <div className="border-2 border-blue-800 rounded-md p-1">Hemsida</div>
-                <div className="border-2 border-blue-800 rounded-md p-1">Instagram</div>
-                <div className="border-2 border-blue-800 rounded-md p-1">Email</div>
+                <div className="border-2 border-blue-800 rounded-md p-1"><a href={restaurant.facebook}>Facebook</a></div>
+                <div className="border-2 border-blue-800 rounded-md p-1"><a href={restaurant.hemsida}>Hemsida</a></div>
+                <div className="border-2 border-blue-800 rounded-md p-1"><a href={restaurant.instagram}>Instagram</a></div>
+                <div className="border-2 border-blue-800 rounded-md p-1"><a href={restaurant.epost}>Email</a></div>
             </div>
             <div className="m-3">
-                <p>Beskrivning</p>
+                <p>Beskrivning:</p>
+                <p>{restaurant.beskrivning}</p>
             </div>
             <br />
-            <p className="m-3">Adress: </p>
-            <p className="m-3">Ort: </p>
-            <p className="m-3">Telefon: </p>
-            <p className="m-3">Vegetarianskt: </p>
+            <p className="m-3">Adress: {restaurant.adress}</p>
+            <p className="m-3">Ort: {restaurant.ort}</p>
+            <p className="m-3">Telefon: {restaurant.telefon}</p>
+            <p className="m-3">Vegetarianskt: {restaurant.vego}</p>
         </div>
     )
 }
--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import RestaurantInfoCard from '../components/RestaurantInfoCard'
+import useRestaurants from '../hooks/useRestaurants'
 
 const RestaurantPage = () => {
+    const restaurant = useRestaurants()
+
     return (
         <div className="container mx-auto flex justify-center">
-            <RestaurantInfoCard />
+            {restaurant.data && restaurant.data.map(res => (
+                <RestaurantInfoCard key={res.id} restaurant={res} />
+            ))}
+            
         </div>
     )
 }
