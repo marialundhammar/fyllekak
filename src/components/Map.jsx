f733e947aa424396c7e017dee55be7de9a2ac6a2
Maria Lundhammar <marialundhammar@gmail.com>
2022-09-22 09:19:40 +0200

    fetching restaurants from firebase

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,43 +1,42 @@
-import React from 'react'
-import { GoogleMap,useJsApiLoader, Marker } from '@react-google-maps/api';
-import useMap from '../hooks/useMap'
-
+import React from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import useMap from "../hooks/useMap";
+import useRestaurants from "../hooks/useRestaurants";
 
 const Map = () => {
+  const showMap = useMap();
 
-  const showMap = useMap()
+  const { data } = useRestaurants();
 
+  console.log(data);
 
-  return showMap.isLoaded && (
-      <GoogleMap
-        mapContainerStyle={ showMap.containerStyle}
-        center={ showMap.coords}
-        zoom={15}
-        >
-       
+  if (data) {
+    data.data.map((res) => console.log("this is the mapping", res.name));
+  }
+ */
+  return (
+    showMap.isLoaded && (
+      <GoogleMap mapContainerStyle={showMap.containerStyle} center={showMap.coords} zoom={15}>
         <Marker
-        icon={{
-          path: google.maps.SymbolPath.CIRCLE,
-          scale: 7,
-         
-        }}
-        position={showMap.coords}
-        label = "Maria"
+          icon={{
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 7,
+          }}
+          position={showMap.coords}
+          label="Maria"
         />
- 
+
         <Marker
-        icon={{
-          path: google.maps.SymbolPath.CIRCLE,
-          scale: 7,
-        }}
-        position={showMap.coords2}
-        label = "Alexander"
+          icon={{
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 7,
+          }}
+          position={showMap.coords2}
+          label="Alexander"
         />
-  
       </GoogleMap>
-  )
- 
- }
- 
- export default Map
-   
+    )
+  );
+};
+
+export default Map;
--- src/hooks/useMap.jsx
+++ src/hooks/useMap.jsx
@@ -1,49 +1,38 @@
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-
-
-
-
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 const useMap = () => {
+  const googleAPI = import.meta.env.VITE_GOOGLE_MAP_API;
+  console.log("this is the api", googleAPI);
 
-    const googleAPI =  import.meta.env.VITE_GOOGLE_MAP_API
-    console.log("this is the api", googleAPI)
-
-    //Just trying with some coords 
-    const coords =  {
+  //Just trying with some coords
+  const coords = {
     lat: 55.593242405381694,
-    lng:13.016352876632064,
-    }
- 
-    //Just trying with some coords 
-    const coords2 =  {
-     lat:55.58477609505269,
-     lng:13.011208857604752,
-    }
+    lng: 13.016352876632064,
+  };
 
-    //style of map container
-   const containerStyle = {
+  //Just trying with some coords
+  const coords2 = {
+    lat: 55.58477609505269,
+    lng: 13.011208857604752,
+  };
+
+  //style of map container
+  const containerStyle = {
     width: "100em",
-    height: '100em'
-    };  
-  
+    height: "100em",
+  };
 
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: `${googleAPI}`
-    })
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: `${googleAPI}`,
+  });
 
- 
-  
-    return {
-        isLoaded,
-        coords, 
-        coords2, 
-        containerStyle
-    }
+  return {
+    isLoaded,
+    coords,
+    coords2,
+    containerStyle,
+  };
+};
 
-
-}
-
-
-export default useMap
+export default useMap;
