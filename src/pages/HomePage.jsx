175deda024de12c890d04215342087a6c39f0d24
Maria Lundhammar <marialundhammar@gmail.com>
2022-09-22 16:29:02 +0200

    trying to get markers to map over map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,53 +1,49 @@
-
 import React from "react";
 import Map from "../components/Map";
 import useRestaurants from "../hooks/useRestaurants";
-
+import { useState } from "react";
 //just for showing cazpian
 
 const HomePage = () => {
   const restaurantQuery = useRestaurants();
 
-
-const HomePage = () => { 
-  const [location, setLocation] = useState()
-
-  
+  const [location, setLocation] = useState();
 
   const getUserLocation = () => {
     if (navigator.geolocation) {
-      console.log('yay')
-      navigator.geolocation.getCurrentPosition(position => {
-
+      console.log("yay");
+      navigator.geolocation.getCurrentPosition((position) => {
         const userLocation = {
           lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
+          lng: position.coords.longitude,
+        };
 
-        console.log('userLocation ==>', userLocation )
+        console.log("userLocation ==>", userLocation);
 
-        setLocation(userLocation)
-        
-      })
-      
+        setLocation(userLocation);
+      });
     } else {
-      console.log('nay')
+      console.log("nay");
     }
-  }
+  };
 
-    return(
-        <>
-        <div className="container mx-auto flex justify-center text-lg">HOMEPAGE</div>
-        <button onClick={() => {getUserLocation()}}>User Location</button>
-        <div className="flex justify-center">
-        <Map userLocation={ location } query={restaurantQuery} />
-        </div>
-      </>
-    )
-  
-}
-
-
-
+  return (
+    <>
+      <div className="container mx-auto flex justify-center text-lg">
+        HOMEPAGE
+      </div>
+      <button
+        onClick={() => {
+          getUserLocation();
+        }}
+      >
+        User Location
+      </button>
+      <div className="flex justify-center">
+        <Map userLocation={location} query={restaurantQuery} />
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
