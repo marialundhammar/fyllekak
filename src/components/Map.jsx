211cdad4cb1f1fbe3ea996a4bf64bb6762e3684d
Alex <alexanderbergquist80@gmail.com>
2022-09-29 15:00:35 +0200

    usestate to custom hook in maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import {
-  GoogleMap,
-  useJsApiLoader,
-  MarkerF,
+	GoogleMap,
+	useJsApiLoader,
+	MarkerF,
 } from "@react-google-maps/api"
 import { useState } from "react"
 import { useEffect } from "react"
@@ -11,65 +11,65 @@ import RestaurantInfoCard from "./RestaurantInfoCard"
 import useRestaurants from "../hooks/useRestaurants"
 
 const Map = ({ location, data, center }) => {
-  const googleAPI = import.meta.env.VITE_GOOGLE_MAP_API;
-  const [restaurants, setRestaurants] = useState([]);
-  const [selectedMarker, setSelectedMarker] = useState(null);
+	const googleAPI = import.meta.env.VITE_GOOGLE_MAP_API;
+	const restaurants = useRestaurants('resturants')
+	const [selectedMarker, setSelectedMarker] = useState(null);
 
-  const containerStyle = {
-    width: "80vw",
-    height: "80vh",
-  }
+	const containerStyle = {
+		width: "80vw",
+		height: "80vh",
+	}
 
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: `${googleAPI}`,
-  })
+	const { isLoaded } = useJsApiLoader({
+		id: "google-map-script",
+		googleMapsApiKey: `${googleAPI}`,
+	})
 
-  if (isLoaded && restaurants) {
-  }
-  return (
-    isLoaded &&
-    restaurants && (
-      <>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={15}
-        >
-          {restaurants.map((restaurant) => (
-            <MarkerF
-              icon={{
-                path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                scale: 5,
-              }}
-              position={restaurant.coords}
-              label={restaurant.name}
-              onClick={() => {
-                setSelectedMarker(restaurant)
-              }}
-              key={restaurant.id}
-            />
-          ))}
+	if (isLoaded && restaurants) {
+	}
+	return (
+		isLoaded &&
+		restaurants && (
+			<>
+				<GoogleMap
+					mapContainerStyle={containerStyle}
+					center={center}
+					zoom={15}
+				>
+					{restaurants.map((restaurant) => (
+						<MarkerF
+							icon={{
+								path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+								scale: 5,
+							}}
+							position={restaurant.coords}
+							label={restaurant.name}
+							onClick={() => {
+								setSelectedMarker(restaurant)
+							}}
+							key={restaurant.id}
+						/>
+					))}
 
-          <MarkerF
-            icon={{
-              path: google.maps.SymbolPath.CIRCLE,
-              scale: 7,
-            }}
-            position={location}
-            label="User Location"
-          />
-        </GoogleMap>
+					<MarkerF
+						icon={{
+							path: google.maps.SymbolPath.CIRCLE,
+							scale: 7,
+						}}
+						position={location}
+						label="User Location"
+					/>
+				</GoogleMap>
 
-        {selectedMarker && (
-          <RestaurantInfoCard
-            key={selectedMarker.id}
-            restaurant={selectedMarker}
-          />
-        )}
-      </>
-    )
-  )
+				{selectedMarker && (
+					<RestaurantInfoCard
+						key={selectedMarker.id}
+						restaurant={selectedMarker}
+					/>
+				)}
+			</>
+		)
+	)
 }
 
 export default Map
