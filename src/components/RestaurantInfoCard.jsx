fe9ab92692070a3ccd0d726729565afe527c34ff
Maria Lundhammar <marialundhammar@gmail.com>
2022-09-30 15:00:41 +0200

    changed class --> className on styling element

--- src/components/RestaurantInfoCard.jsx
+++ src/components/RestaurantInfoCard.jsx
@@ -56,7 +56,7 @@ const RestaurantInfoCard = ({ restaurant }) => {
 					</button>
 					{lat && (
 						<div
-							class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
+							className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
 							role="alert"
 						>
 							<p>
