a8ad0d5247472efea872f4a119105dc56d698c1e
Alex <alexanderbergquist80@gmail.com>
2022-10-03 13:49:24 +0200

    push for maria

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -10,22 +10,26 @@ import { useFirestoreQueryData } from '@react-query-firebase/firestore'
 
 const RestaurantList = () => {
 	const [infoCard, setInfoCard] = useState()
+	const [vego, setVego] = useState(false)
+	const [pricerange, setPricerange] = useState('2')
+
 	// const restaurant = useRestaurants('restaurants')
 
 	const { currentUser } = useAuthContext()
 
 	const queryRef = query(
 		collection(db, 'restaurants'),
-		where('type', '==', 'snabbmat'),
-		orderBy('name')
+		// where('vego', '==', vego),
+		where('price', '==', pricerange),
+		// orderBy('name')
 	)
 
-	const { data: restaurant, isLoading, error } = useFirestoreQueryData(['restaurants'
-		// , { uid: currentUser.uid }
-	], queryRef, {
-		idField: 'id',
-		subscribe: true,
-	})
+	const { data: restaurant, isLoading, error } = useFirestoreQueryData(['restaurants', { vego }], queryRef,
+		{
+			// 	idField: 'id',
+			subscribe: true,
+		}
+	)
 
 	const toggleCard = (res) => {
 		if (infoCard == res) setInfoCard();
@@ -35,24 +39,36 @@ const RestaurantList = () => {
 	return (
 		<div className="flex">
 
+			<div>
+				<button onClick={() => setVego(vego ? false : true)}> Vegetariskt </button>
+			</div>
+			<div>
+				<button onClick={() => {
+					console.log(pricerange)
+					setPricerange('1')
+					console.log(pricerange)
+				}}> Billigt </button>
+
+				<button onClick={() => setPricerange('2')}> Mellan </button>
+				<button onClick={() => setPricerange('3')}> Dyrt </button>
+			</div>
+
 			{isLoading && <p>Loading...</p>}
 
 			{error && <p>{error.message}</p>}
 
+			{restaurant && console.log(restaurant)}
+
 			{restaurant && (
 				<ul className="bg-blue-400 w-1/5">
-					{restaurant.data &&
-						restaurant.data.map((res) => (
+					{restaurant &&
+						restaurant.map((res) => (
 							<div
 								className="border border-blue-800 flex justify-between"
-								key={res.id}
+								key={restaurant.indexOf(res)}
 							>
-								<li className="p-2" onClick={() => toggleCard(res)}>
-									{res.name}
-								</li>
-								<a className="p-2 text-blue-900" href={res.website}>
-									Website
-								</a>
+								<li className="p-2" onClick={() => toggleCard(res)}>{res.name}</li>
+								<a className="p-2 text-blue-900" href={res.website}>Website</a>
 							</div>
 						))}
 				</ul>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RestaurantList from './RestaurantList'
+
+const Sidebar = () => {
+	return (
+		<>
+
+			<RestaurantList />
+		</>
+	)
+}
+
+export default Sidebar
