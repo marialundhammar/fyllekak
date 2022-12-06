82765f516826547c5d278d0e9a48a5f9f292beea
Maria Lundhammar <marialundhammar@Marias-MacBook-Pro.local>
2022-12-06 22:17:54 +0100

    fixed mapCenter

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,108 +1,89 @@
-import { useState } from "react"
-import {
-	GoogleMap,
-	useJsApiLoader,
-	MarkerF,
-} from "@react-google-maps/api"
-import RestaurantInfoCard from "./RestaurantInfoCard"
-import googleMapsStyle from "../googleMapsStyle"
-import SearchBar from "../components/SearchBar"
-import { useAuthContext } from "../contexts/AuthContext"
+import { useState, useEffect } from "react";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import googleMapsStyle from "../googleMapsStyle";
+import SearchBar from "../components/SearchBar";
+import { useAuthContext } from "../contexts/AuthContext";
 
 const Map = ({ location, restaurants }) => {
-	const googleAPI = import.meta.env.VITE_GOOGLE_MAP_API
+  const googleAPI = import.meta.env.VITE_GOOGLE_MAP_API;
 
-	const { setInfoCardRestaurant } = useAuthContext()
+  const { mapCenter, setMapCenter, setInfoCardRestaurant } = useAuthContext();
 
-	const containerStyle = {
-		width: "100%",
-		height: "100%",
-	}
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+  };
 
-	const [mapCenter, setMapCenter] = useState({
-		lat: 55.59712105786678,
-		lng: 12.997431424230891,
-	})
+  const [libraries] = useState(["places"]);
 
-	const [libraries] = useState(["places"])
+  const mapStyle = googleMapsStyle;
 
-	const mapStyle = googleMapsStyle
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: `${googleAPI}`,
+    libraries,
+  });
 
-	const { isLoaded } = useJsApiLoader({
-		id: "google-map-script",
-		googleMapsApiKey: `${googleAPI}`,
-		libraries,
-	})
+  const gMaps = isLoaded ? window.google.maps : {};
 
-	const gMaps = isLoaded ? window.google.maps : {}
+  const iconRestaurant = isLoaded
+    ? {
+        path: "M12,2a8.009,8.009,0,0,0-8,8c0,3.255,2.363,5.958,4.866,8.819,0.792,0.906,1.612,1.843,2.342,2.791a1,1,0,0,0,1.584,0c0.73-.948,1.55-1.885,2.342-2.791C17.637,15.958,20,13.255,20,10A8.009,8.009,0,0,0,12,2Zm0,11a3,3,0,1,1,3-3A3,3,0,0,1,12,13Z",
+        fillColor: "#42f5c5",
+        fillOpacity: 1,
+        strokeOpacity: 0,
+        anchor: new gMaps.Point(12, 22),
+        scale: 1.5,
+        labelOrigin: new gMaps.Point(10, -8),
+      }
+    : {};
 
-	const iconRestaurant = isLoaded
-		? {
-				path:
-					"M12,2a8.009,8.009,0,0,0-8,8c0,3.255,2.363,5.958,4.866,8.819,0.792,0.906,1.612,1.843,2.342,2.791a1,1,0,0,0,1.584,0c0.73-.948,1.55-1.885,2.342-2.791C17.637,15.958,20,13.255,20,10A8.009,8.009,0,0,0,12,2Zm0,11a3,3,0,1,1,3-3A3,3,0,0,1,12,13Z",
-				fillColor: "#42f5c5",
-				fillOpacity: 1,
-				strokeOpacity: 0,
-				anchor: new gMaps.Point(12, 22),
-				scale: 1.5,
-				labelOrigin: new gMaps.Point(10, -8),
-		  }
-		: {}
+  const iconUser = {
+    path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
+    fillColor: "#42b9f5",
+    fillOpacity: 0.6,
+    strokeWeight: 0,
+    scale: 1.1,
+  };
 
-	const iconUser = {
-		path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
-		fillColor: "#42b9f5",
-		fillOpacity: 0.6,
-		strokeWeight: 0,
-		scale: 1.1,
-	}
+  return (
+    isLoaded &&
+    restaurants && (
+      <>
+        <div className="flex flex-col w-screen h-screen z-0">
+          <div className=" w-full">
+            <SearchBar setMapCenter={setMapCenter} />
+          </div>
 
-	return (
-		isLoaded &&
-		restaurants && (
-			<>
-				<div className="flex flex-col w-screen h-screen z-0">
-					<div className=" w-full">
-						<SearchBar setMapCenter={setMapCenter} />
-					</div>
+          <div className="flex justify-center items-center flex-grow">
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={mapCenter}
+              zoom={15}
+              options={{ styles: mapStyle }}
+            >
+              {restaurants.map((restaurant) => (
+                <MarkerF
+                  position={restaurant.coords}
+                  label={{
+                    text: restaurant.name,
+                    className: "labelStyle",
+                  }}
+                  onClick={() => {
+                    setInfoCardRestaurant(restaurant);
+                    setMapCenter(restaurant.coords);
+                  }}
+                  key={restaurant.id}
+                  icon={iconRestaurant}
+                />
+              ))}
+              <MarkerF icon={iconUser} position={location} animation={1} />
+            </GoogleMap>
+          </div>
+        </div>
+      </>
+    )
+  );
+};
 
-					<div className="flex justify-center items-center flex-grow">
-						<GoogleMap
-							mapContainerStyle={containerStyle}
-							center={mapCenter}
-							zoom={15}
-							options={{ styles: mapStyle }}
-						>
-							{restaurants.map((restaurant) => (
-								<MarkerF
-									position={restaurant.coords}
-									label={{
-										text: restaurant.name,
-										className: "labelStyle",
-									}}
-									onClick={() => {
-										setInfoCardRestaurant(
-											restaurant
-										)
-										setMapCenter(
-											restaurant.coords
-										)
-									}}
-									key={restaurant.id}
-									icon={iconRestaurant}
-								/>
-							))}
-							<MarkerF
-								icon={iconUser}
-								position={location}
-								animation={1}
-							/>
-						</GoogleMap>
-					</div>
-				</div>
-			</>
-		)
-	)
-}
-
-export default Map
+export default Map;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,131 +1,124 @@
-import { useEffect, useState } from "react"
-import { useSearchParams } from "react-router-dom"
-import { useAuthContext } from "../contexts/AuthContext"
-import useRestaurants from "../hooks/useRestaurants"
-import RestaurantList from "../components/RestaurantList"
-import Map from "../components/Map"
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useAuthContext } from "../contexts/AuthContext";
+import useRestaurants from "../hooks/useRestaurants";
+import RestaurantList from "../components/RestaurantList";
+import Map from "../components/Map";
 
 const HomePage = () => {
-	const [query, setQuery] = useState(null)
-	const [filteredRestaurants, setFilteredRestaurants] = useState([])
+  const [query, setQuery] = useState(null);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const { setMapCenter } = useAuthContext();
 
-	const { infoCardRestaurant } = useAuthContext()
+  const [searchParams, setSearchParams] = useSearchParams({
+    filter: "all",
+  });
 
-	const [searchParams, setSearchParams] = useSearchParams({
-		filter: "all",
-	})
+  const { data: restaurants, isLoading } = useRestaurants("restaurants");
 
-	const { data: restaurants, isLoading } = useRestaurants(
-		"restaurants"
-	)
+  const handleFilter = (query) => {
+    if (query === "all") {
+      setFilteredRestaurants(restaurants);
+      setSearchParams({ filter: "all" });
+      return;
+    }
 
-	const handleFilter = (query) => {
-		if (query === "all") {
-			setFilteredRestaurants(restaurants)
-			setSearchParams({ filter: "all" })
-			return
-		}
+    const filteredArray = restaurants.filter((res) => res[query]);
+    console.log("this is query", query);
+    console.log("this is query", filteredArray);
+    setFilteredRestaurants(filteredArray);
+    setSearchParams({ filter: query });
+  };
 
-		const filteredArray = restaurants.filter((res) => res[query])
-		setFilteredRestaurants(filteredArray)
-		setSearchParams({ filter: query })
-	}
+  const [location, setLocation] = useState({
+    lat: 55.59712105786678,
+    lng: 12.997431424230891,
+  });
 
-	const [location, setLocation] = useState({
-		lat: 55.59712105786678,
-		lng: 12.997431424230891,
-	})
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const userLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
 
-	const getUserLocation = () => {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition((position) => {
-				const userLocation = {
-					lat: position.coords.latitude,
-					lng: position.coords.longitude,
-				}
+        setLocation(userLocation);
+        setMapCenter(userLocation);
+      });
+    } else {
+      return;
+    }
+  };
 
-				setLocation(userLocation)
-			})
-		} else {
-			return
-		}
-	}
+  useEffect(() => {
+    if (isLoading) return;
 
-	useEffect(() => {
-		if (isLoading) return
+    if (searchParams.get("filter")) {
+      handleFilter(searchParams.get("filter"));
+    }
+    getUserLocation();
+  }, [query, isLoading]);
 
-		if (searchParams.get("filter")) {
-			handleFilter(searchParams.get("filter"))
-		}
-		getUserLocation()
-	}, [query, isLoading])
+  return (
+    filteredRestaurants && (
+      <>
+        <div className="flex flex-col-reverse md:flex-row bg-darkish-blue text-contrast-color">
+          {/* Sidebar */}
+          <div className="w-full sm:w-1/4 p-2">
+            <div>
+              <div className="ui-sans-serif flex flex-col">
+                <button
+                  className="p-2 border rounded border-contrast-color hover:border-contrast-color-dark hover:text-contrast-color-dark"
+                  onClick={() => {
+                    query !== "vego"
+                      ? handleFilter("vego") && setQuery("vego")
+                      : handleFilter("") && setQuery(null);
+                  }}
+                >
+                  Vegetariskt
+                </button>
 
-	return (
-		filteredRestaurants && (
-			<>
-				<div className="flex flex-col-reverse md:flex-row bg-darkish-blue text-contrast-color">
-					{/* Sidebar */}
-					<div className="w-full sm:w-1/4 p-2">
-						<div>
-							<div className="ui-sans-serif flex flex-col">
-								<button
-									className="p-2 border rounded border-contrast-color hover:border-contrast-color-dark hover:text-contrast-color-dark"
-									onClick={() => {
-										query !== "vego"
-											? handleFilter("vego") &&
-											  setQuery("vego")
-											: handleFilter("") &&
-											  setQuery(null)
-									}}
-								>
-									Vegetariskt
-								</button>
+                <button
+                  className="p-2 border rounded border-contrast-color hover:border-contrast-color-dark hover:text-contrast-color-dark"
+                  onClick={() => {
+                    query !== "price"
+                      ? handleFilter("price") && setQuery("price")
+                      : handleFilter("") && setQuery(null);
+                  }}
+                >
+                  Billigt
+                </button>
 
-								<button
-									className="p-2 border rounded border-contrast-color hover:border-contrast-color-dark hover:text-contrast-color-dark"
-									onClick={() => {
-										query !== "price"
-											? handleFilter("price") &&
-											  setQuery("price")
-											: handleFilter("") &&
-											  setQuery(null)
-									}}
-								>
-									Billigt
-								</button>
+                <button
+                  className="p-2 border rounded border-contrast-color hover:border-contrast-color-dark hover:text-contrast-color-dark"
+                  onClick={() => {
+                    query !== "all"
+                      ? handleFilter("all") && setQuery("all")
+                      : handleFilter("") && setQuery(null);
+                  }}
+                >
+                  Alla
+                </button>
+              </div>
+            </div>
 
-								<button
-									className="p-2 border rounded border-contrast-color hover:border-contrast-color-dark hover:text-contrast-color-dark"
-									onClick={() => {
-										query !== "all"
-											? handleFilter("all") &&
-											  setQuery("all")
-											: handleFilter("") &&
-											  setQuery(null)
-									}}
-								>
-									Alla
-								</button>
-							</div>
-						</div>
+            <div className="my-2">
+              <RestaurantList restaurants={filteredRestaurants} />
+            </div>
+          </div>
 
-						<div className="my-2">
-							<RestaurantList
-								restaurants={filteredRestaurants}
-							/>
-						</div>
-					</div>
+          {/* Maps component */}
 
-					{/* Maps component */}
-					<Map
-						className="w-full sm:w-3/4 h-full"
-						location={location}
-						restaurants={filteredRestaurants}
-					/>
-				</div>
-			</>
-		)
-	)
-}
+          <Map
+            className="w-full sm:w-3/4 h-full"
+            location={location}
+            restaurants={filteredRestaurants}
+          />
+        </div>
+      </>
+    )
+  );
+};
 
-export default HomePage
+export default HomePage;
